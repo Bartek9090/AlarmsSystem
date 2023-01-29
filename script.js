f469af017580483cf0ebfbdc0b076be4d72dca7d
Bartek Bartosik <b.bartek987@gmail.com>
2023-01-29 20:18:58 +0000

    first mobile finished?

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const menu = document.querySelector(".hamburger-menu")
 menu.addEventListener('click', ()=>{
 	document.querySelector(".topbar").classList.toggle('change')
 })
+
+// slider
+
+const images = ["img/dvbtsat.jpg", "IMG/tvprze.jpg", "IMG/videodomofon.jpg"];
+const carousel = document.querySelector('.carousel')
+const interval = setInterval(function() {
+	startCarousel()
+}, 4000);
+
+let index = 1;
+
+startCarousel =  () =>{ 
+	carousel.style.backgroundImage = `url(${images[index++]})`;
+
+	carousel.classList.remove('fade');
+	void carousel.offsetWidth;
+	if(index > images.length -1  ) index = 0;
+	carousel.classList.add('fade')
+}
