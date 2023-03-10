a0a42ac3378334ac8c9242c7f144c57f56ad88fa
Bartek Bartosik <b.bartek987@gmail.com>
2023-03-10 17:37:36 +0000

    scaling images

--- lightboxRealization.js
+++ lightboxRealization.js
@@ -40,5 +40,5 @@ function showSlide(n) {
   };
   
   slides[slideIndex - 1].style.display = 'block';
-  modalPreviews[slideIndex - 1].className += ' active';
+  modalPreviews[slideIndex - 1].className += 'active';
 };
